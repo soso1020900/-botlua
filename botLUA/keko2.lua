68e2c58f9c68b4bdf58653c8cb2049c6a19b0814
kekobot <vejegaz@9me.site>
2017-11-07 05:27:26 +0300

    Update keko2.lua

--- botLUA/keko2.lua
+++ botLUA/keko2.lua
@@ -6,22 +6,17 @@ local action = function(msg, matches)
 	
 if is_admin(msg) then	
 	if matches[1] == 'bc' then
-		local users = client:smembers('keko')
-		local text = ''
-		if not users then
-		text = 'لا يوجد اعضاء'
-		end
+		local users = client:smembers('botlua'..bot.id)
 		for i=1, #users do
 		api.sendMessage(users[i], matches[2], true)
 		print(colors('%{green bright}ارسال الى : '), users[i])
-		text = 'تم النشر الرساله الى جميع الاعضاء'
 		end
-		api.sendReply(msg, text, true)
+		api.sendReply(msg, 'تم النشر الرساله الى جميع الاعضاء', true)
 	end
 
 
  if matches[1] == 'stats' then
-		local members = client:smembers('keko')
+		local members = client:smembers('botlua'..bot.id)
 		local keko = ''
 		if members then
 			for i=1, #members do
